5aa13b3cf52aada512680559c4a95bf7beb4b723
Mutyala Harshith <105526704+Mega-Hero@users.noreply.github.com>
2022-06-26 15:47:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jeolpaul/TG-MULTI-BOT.git /TG-MULTI-BOT     
+  git clone https://github.com/Mega-Hero/Graph.git /graph     
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TG-MULTI-BOT
+  git clone $UPSTREAM_REPO /graph
 fi
-cd /TG-MULTI-BOT
+cd /graph
 pip3 install -U -r requirements.txt
 echo "BOT IS STARTING⚡️⚡️⚡️"
 python3 config.py
